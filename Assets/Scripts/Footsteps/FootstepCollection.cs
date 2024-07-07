c1d73e94e707ae7cfc2d63773ea634be296987a3
Aleksandar Bankov <ge65hod@mytum.de>
2024-07-07 03:33:14 +0200

    Zwischenabgabe Implementation

---
+++ Assets/Scripts/Footsteps/FootstepCollection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Footstep Collection", menuName ="Create New Footstep Collection")]
+
+public class FootstepCollection : ScriptableObject
+{
+    public List<AudioClip> footstepSounds = new List<AudioClip>();
+    public AudioClip jumpSound;
+    public AudioClip landSound;
+
+}
